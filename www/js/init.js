c692920dc2254b2c5066c6828d54ec40dad36a9d
dmitry.arefyev <seet_seet@mail.ru>
2017-06-22 22:13:40 +0300

    add menu

--- www/js/init.js
+++ www/js/init.js
@@ -6,9 +6,9 @@
 
 var width = window.innerWidth;
 var height = window.innerHeight;
-var game = new Phaser.Game(width, height, Phaser.CANVAS, 'joy',
-    { preload: preload, create: create, update: update, render: render  });
 
+var game = new Phaser.Game(width, height, Phaser.CANVAS, 'joy',
+    { preload: preload, create: create, update: update, render: render });
 
 function preload() {
     game.load.image('menu_button', 'img/flixel-button.png');
@@ -20,6 +20,10 @@ function preload() {
 function create() {
     game.stage.backgroundColor = '#337799';
 
+    var welcome = game.add.bitmapText(width*0.5, height*0.5, 'desyrel', 'Joy', 64);
+    welcome.anchor.x = 0.5;
+    welcome.anchor.y = 0.5;
+
     //Кнопка меню и ее события
     menu_button = game.add.sprite( width - 100, 10, 'menu_button');
     menu_button.inputEnabled = true;
@@ -30,6 +34,7 @@ function create() {
         //Добавляем содержимое меню
         menu_items = game.add.sprite(width/2, height/2, 'menu_items');
         menu_items.anchor.setTo(0.5, 0.5);
+
 
         //Добавляем выбор элемента
         //choiseLabel = game.add.text(width/2, height-150, 'Нажмите вне меню для продолжения игры', { font: '30px Arial', fill: '#fff' });
@@ -47,23 +52,26 @@ function create() {
 
             //Если нажали внутри меню
             if (event.x > x1 && event.x < x2 && event.y > y1 && event.y < y2) {
-                var choisemap = ['numbers', 'two', 'three', 'four', 'five', 'six'];
+                var choiсemap = ['numbers', 'pyramid', 'color', 'four', 'five', 'six'];
                 //Координаты нажатия
                 var x = event.x - x1,
                     y = event.y - y1;
 
                 //Вычисляем выбор
-                var choise = Math.floor(x / 90) + 3 * Math.floor(y / 90);
+                var choiсe = Math.floor(x / 90) + 3 * Math.floor(y / 90);
 
                 //Результат
-                console.log('Вы выбрали игру: ' + choisemap[choise]);
+                console.log('Вы выбрали игру: ' + choiсemap[choiсe]);
                 //choiseLabel.text = 'Вы выбрали игру: ' + choisemap[choise];
-                if (choisemap[choise] == 'numbers') {
+                if (choiсemap[choiсe] == 'numbers') {
                     menu_items.destroy();
                     game.paused = false;
-                    var numbers = new numbers(game);
-                    numbers.anchor.setTo(0.5, 0.5);
-
+                    var numbers = new Numbers(game);
+                    welcome.destroy();
+                } else {
+                    menu_items.destroy();
+                    game.paused = false;
+                    welcome.destroy();
                 }
             } else {
                 //Просто нажатие вне меню
